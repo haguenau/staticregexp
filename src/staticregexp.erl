5a5ab9a9f96d790cc898292b4ed1f475bfdfdf96
David Haguenauer <david@adgear.com>
2015-06-19 16:39:49 -0400

    Downgraded parse_trans to 2.8 to work around version conflict. Added actual code.

--- src/staticregexp.erl
+++ src/staticregexp.erl
@@ -1,9 +1,27 @@
 -module(staticregexp).
 -export([parse_transform/2]).
 
--record(state, {}).
+parse_transform(Forms, _Options) ->
+  parse_trans:plain_transform(fun apply_transform/1, Forms).
 
-parse_transform(Forms, Options) ->
-  parse_trans:transform(fun do_transform/4, #state{}, Forms, Options).
+apply_transform(
+    {call, Line,
+      {remote, _, {atom, _, staticregexp}, {atom, _, compile}},
+      [{string, _, String}]}) ->
+      Compiled = re:compile(String),
+      case Compiled of
+        {ok, Regexp} ->
+          erl_parse:abstract(Regexp, Line);
+        _ ->
+          io:format(standard_error, "staticregexp: Call to re:compile failed: ~p~n", [Compiled]),
+          invalid
+      end;
 
-do_transform(_Type, Form, _Context, State) -> {Form, false, State}.
+apply_transform({call, _, {remote, _, {atom, _, staticregexp}, {atom, _, compile}}, Params}) ->
+  NormalisedParams = lists:map(fun erl_parse:normalise/1, Params),
+  io:format(standard_error, "staticregexp: Bad parameters to staticregexp:compile: ~p~n",
+    [NormalisedParams]),
+  invalid;
+
+apply_transform(_Form) ->
+  continue.
